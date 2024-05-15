005da81cae0043397d242f5bf3fc09b76aa96113
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2024-05-15 11:20:12 +0400

    Добавьте файлы проекта.

---
+++ LinqMethods/ExtensionLinq.cs
@@ -0,0 +1,36 @@
+﻿namespace LinqMethods
+{
+    public static class ExtensionLinq
+    {
+        public static IEnumerable<T> WhereLazy<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> SelectLazy<T, TResult>(this IEnumerable<T> source, Func<T, TResult> predicate)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+
+            foreach (var item in source)
+            {
+                yield return predicate(item);
+            }
+        }
+
+        public static List<T> ToListMaterial<T>(this IEnumerable<T> source)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+            List<T> list = [.. source];
+            return list;
+        }
+    }
+}
---
+++ LinqMethods/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace LinqMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Select
+            var people = new List<Person>
+            {
+                new Person ("Tom", 23),
+                new Person ("Bob", 27),
+                new Person ("Sam", 29),
+            };
+
+            var names = people.SelectLazy(p => p.Name);
+            people.Add(new Person("Alice", 24));
+
+            foreach (string n in names)
+                Console.WriteLine(n);
+
+            Console.WriteLine("-------");
+
+            //Where
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+          
+            var source = list.WhereLazy(item => item % 2 == 0);
+            list.Add(6);
+            foreach (var item in source)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------");
+
+           
+            //ToList
+            var listSource = list.WhereLazy(item => item % 2 == 0).ToListMaterial();
+            list.Add(8);
+            foreach (var item in listSource)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
+record class Person(string Name, int Age);
